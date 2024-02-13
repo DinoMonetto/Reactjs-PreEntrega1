1d3a9fb8b37892ee1a800f66eb3d6f645a8c28b2
dino monetto <dinolmonetto@gmail.com>
2024-02-13 11:30:05 -0300

    Finalizado?2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './App.css'
 import NavBar from "./components/NavBar/NavBar.jsx";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>MiniMercado</h1>
       <div className="card">
         <NavBar/>
-        <ItemListContainer greeting={Bienvenidos}/>
+        <ItemListContainer greeting="Bienvenidos"/>
       </div>
     </>
   )
